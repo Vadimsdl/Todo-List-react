601bdfd886b4b24cde66a0dcb20ecb9c911976e7
Nikita Vasyutchenko <nikita.vasyutchenko@qbex.io>
2021-08-13 14:39:37 +0300

    finaly

--- src/component/Sublist.js
+++ src/component/Sublist.js
@@ -2,20 +2,19 @@ import React from 'react';
 import UserList from './UserList'
 
 function Sublist({taskTitle, task, deleteSublist, tasks, addListElem, removeTask, setUp, setDown, getIndex, setIndex }) {
-  let getTasks = task.sublist;
 
   return (
     <li >
       <div className="title-block">
       <span className="title-task">{taskTitle}</span>
       </div>
-      <button className="btn" type="button" onClick={() => setUp(task.id)}>Up</button>
-      <button className="btn" type="button" onClick={() => setDown(task.id)}>Down</button>
+      <button className="btn" type="button" onClick={() => setUp(task.id, task.parentId)}>Up</button>
+      <button className="btn" type="button" onClick={() => setDown(task.id, task.parentId)}>Down</button>
       <button className="btn" type="button" onClick={() => removeTask(task.id)}>Delete</button>
       <button className="btn" type="button" onClick={() => deleteSublist(task.id)}>Remove Sublist</button>
         <UserList
           addListElem={addListElem}
-          tasks={getTasks}
+          tasks={task.sublist}
           id={task.id}
           taskTitle={taskTitle}
           deleteSublist={deleteSublist}
